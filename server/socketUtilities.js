6e74d438c9f74100bb1dc56c2fd6b56937d966ea
Alan-James-Campbell <pi@raspberrypi>
2020-04-19 20:12:55 -0600

    led on works

--- server/socketUtilities.js
+++ server/socketUtilities.js
@@ -1,4 +1,4 @@
-// const Gpio = require('../pigpio').Gpio
+const Gpio = require('pigpio').Gpio
 
 const addSocketListeners = (socket, io) => {
   socket.on('disconnect', () => {
@@ -9,34 +9,34 @@ const addSocketListeners = (socket, io) => {
     io.emit('LED-STATUS', res)
   })
 
-  // socket.on('LED-STATUS-CHANGE', (res) => {
-  //   if(res === true){
-  //     const red = new Gpio(17, {mode: Gpio.OUTPUT})
-  //     const green = new Gpio(18, {mode: Gpio.OUTPUT})
+   socket.on('LED-STATUS-CHANGE', (res) => {
+     if(res === true){
+       const red = new Gpio(17, {mode: Gpio.OUTPUT})
+       const green = new Gpio(18, {mode: Gpio.OUTPUT})
       
-  //     let dutyCycle = 0
-  //     let redOn = true
+       let dutyCycle = 0
+       let redOn = true
       
-  //     setInterval(() => {
-  //       dutyCycle += 5
-  //       if (dutyCycle > 255) {
-  //         dutyCycle = 0
-  //         if(redOn){
-  //           red.pwmWrite(dutyCycle)
-  //           redOn = false
-  //         }else {
-  //           green.pwmWrite(dutyCycle)
-  //           redOn = true
-  //         }
-  //       }
-  //       if(redOn){
-  //         red.pwmWrite(dutyCycle)
-  //       }else {
-  //         green.pwmWrite(dutyCycle)
-  //       }  
-  //     }, 20)
-  //   }
-  // })
+       setInterval(() => {
+         dutyCycle += 5
+         if (dutyCycle > 255) {
+           dutyCycle = 0
+           if(redOn){
+             red.pwmWrite(dutyCycle)
+             redOn = false
+           }else {
+             green.pwmWrite(dutyCycle)
+             redOn = true
+           }
+         }
+         if(redOn){
+           red.pwmWrite(dutyCycle)
+         }else {
+           green.pwmWrite(dutyCycle)
+         }  
+       }, 20)
+     }
+   })
 }
 
 module.exports = { addSocketListeners }
